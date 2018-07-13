e718c97502365d7b54fca89328706fbfcab6e1a3
Todd <tpolasek@gmail.com>
2018-07-13 15:15:53 -0700

    int commit

---
+++ content/meta/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: "The Sleep Nomad | On the trail to better sleep", // <title>
+  shortSiteTitle: "The Sleep Nomad", // <title> ending for posts and pages
+  siteDescription: "A sleep blog about improving sleep apnea.",
+  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "The Sleep Nomad",
+  authorTwitterAccount: "sleepnomad",
+  // info
+  headerTitle: "The Sleep Nomad",
+  headerSubTitle: "A Sleep, Sleep Apnea & Insomia blog",
+  // manifest.json
+  manifestName: "The Sleep Nomad",
+  manifestShortName: "SleepNomad", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
---
+++ content/parts/footnote.md
@@ -0,0 +1,8 @@
+---
+title: footnote
+---
+
+- built by [The Sleep Nomad](https://www.TheSleepNomad.com)
+- inspired by [Greg Lobinksi](https://www.greglobinski.com)
+- deliverd by [Netlify](https://www.netlify.com/)
+- photos by [unsplash.com](https://unsplash.com)
