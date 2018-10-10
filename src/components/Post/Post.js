ab935d1bf81b3ff6c9e97723be0bfae8e46d31e5
Todd <tpolasek@gmail.com>
2018-10-10 16:25:07 -0700

    added disqus comments

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -7,7 +7,7 @@ import Headline from "../Article/Headline";
 import Bodytext from "../Article/Bodytext";
 import Meta from "./Meta";
 import Author from "./Author";
-// import Comments from "./Comments";
+import Comments from "./Comments";
 import NextPrev from "./NextPrev";
 
 const Share = asyncComponent(() =>
@@ -43,7 +43,7 @@ const Post = props => {
         <Share post={post} theme={theme} />
         <Author note={authornote} theme={theme} />
         <NextPrev next={nextPost} prev={prevPost} theme={theme} />
-        {/* <Comments slug={slug} facebook={facebook} theme={theme} /> */}
+        <Comments slug={slug} title={title} theme={theme} post={post} />
       </footer>
     </React.Fragment>
   );
@@ -52,7 +52,6 @@ const Post = props => {
 Post.propTypes = {
   post: PropTypes.object.isRequired,
   authornote: PropTypes.string.isRequired,
-  // facebook: PropTypes.object.isRequired,
   next: PropTypes.object,
   prev: PropTypes.object,
   theme: PropTypes.object.isRequired
